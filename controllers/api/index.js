d36b9b050246cab28adab30d7fbe8085b6aa83d0
JP Eiler <jp.eiler97@gmail.com>
2021-05-18 11:11:05 -0500

    added files for api routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 const messageRoutes = require('./messageRoutes');
+const userRoutes = require('./userRoutes');
+const roleRoutes = require('./roleRoutes');
+const tagRoutes = require('./tagRoutes');
 
 router.use('/messages', messageRoutes);
+router.use('/users', userRoutes);
+router.use('/roles', roleRoutes);
+router.use('/tags', tagRoutes);
 
 module.exports = router;
