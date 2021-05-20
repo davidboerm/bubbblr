9d69e0ce8aecad6492b4bf2306d1100b1cd61424
Cunnjoel <cunningham.joel84@gmail.com>
2021-05-19 20:05:28 -0500

    adding to fix merge

---
+++ public/js/login.js
@@ -0,0 +1,50 @@
+const loginFormHandler = async (event) => {
+	event.preventDefault();
+
+	const email = document.querySelector('#email-login').value.trim();
+	const password = document.querySelector('#password-login').value.trim();
+
+	if (email && password) {
+		const response = await fetch('/api/users/login', {
+			method: 'POST',
+			body: JSON.stringify({ email, password }),
+			headers: { 'Content-Type': 'application/json' }
+		});
+
+		if (response.ok) {
+			document.location.replace('/');
+		} else {
+			alert('Failed to log in.');
+		}
+	}
+};
+
+const signupFormHandler = async (event) => {
+	event.preventDefault();
+
+	const name = document.querySelector('#username-signup').value.trim();
+	const email = document.querySelector('#email-signup').value.trim();
+	const password = document.querySelector('#password-signup').value.trim();
+	const leaderCheck = document.querySelector('#leader-check');
+	let isLeader = false;
+	if (leaderCheck.checked) {
+		isLeader = true;
+	}
+	if (name && email && password) {
+		const response = await fetch('/api/users', {
+			method: 'POST',
+			body: JSON.stringify({ name, email, password, isLeader }),
+			headers: { 'Content-Type': 'application/json' }
+		});
+
+		if (response.ok) {
+			document.location.replace('/');
+		} else {
+			alert('Failed to sign up.');
+		}
+	}
+};
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
