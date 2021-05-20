1452989796a0ba753b26e74460c018c4a3a6b8a3
JP Eiler <jp.eiler97@gmail.com>
2021-05-20 18:40:34 -0500

    worked on project setup

--- controllers/routes.js
+++ controllers/routes.js
@@ -44,6 +44,26 @@ router.get('/setup', withAuth, async (req, res) => {
 	}
 });
 
+router.get('/chat', withAuth, async (req, res) => {
+	try {
+		const messageData = await Message.findAll({
+			include: [
+				{
+					model: User,
+					exclude: [ 'password' ]
+				}
+			],
+			order: [ [ 'updatedAt', 'DESC' ] ]
+		});
+
+		const messages = messageData.map((message) => message.get({ plain: true }));
+		res.render('chat', { messages, logged_in: req.session.logged_in, user_id: req.session.user_id });
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
+});
+
 router.get('/project/:id', withAuth, async (req, res) => {
 	try {
 		const projectData = await Project.findByPk(req.params.id, {
