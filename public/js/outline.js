8a99a7008cd2d93fde60d0d84d51accf9845577a
Cunnjoel <cunningham.joel84@gmail.com>
2021-05-21 16:36:45 -0500

    pulling from repo

--- public/js/outline.js
+++ public/js/outline.js
@@ -2,7 +2,7 @@ const { response } = require("express");
 
 async function outlineHandeler(project) {
 	let projectName;
-	const response = await fetch(`/api/projects/${project.message_id}`, {
+	const response = await fetch(`/api/project/${project.message_id}`, {
 		method: 'GET',
 		headers: { 'Content-Type': 'application/json' }
 	})
@@ -15,3 +15,20 @@ async function outlineHandeler(project) {
     <p>  ${project.message} </p>
     `);
 }
+
+async function outlineMessageHandeler(message) {
+	let messageText;
+	const response = await fetch(`/api/projects/${message.chat_text}`, {
+		method: 'GET',
+		headers: { 'Content-Type': 'application/json' }
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			userName = data.name
+		});
+	$('#messages').prepend(`
+		<h4> ${message} <h4>
+	`)
+}
+
+outlineMessageHandeler();
